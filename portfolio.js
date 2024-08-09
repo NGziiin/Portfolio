9d9aab923016ad7f466c1e702174fd776f110862
Herick <herickminecraftano@gmail.com>
2024-08-08 22:48:52 -0300

    .

--- portfolio.js
+++ portfolio.js
@@ -1,5 +1,19 @@
 // configurando botões
 
-function view1bt(){
-    window.open("homer_simpson.html");
+function view1bt(homer){
+
+    if (homer == 1){
+        window.open("homer_simpson.html");
+    }
+    else if (homer == 2){
+        window.open("https://github.com/NGziiin/HTML-design/blob/main/homer%20simpson.html");
+    }
+}
+function view2bt(bart){
+    if (bart == 1){
+        alert("PROJETO NÃO FINALIZADO");
+    }
+    else if(bart == 2){
+        alert("PROJETO NÃO FINALIZADO");
+    }
 }
