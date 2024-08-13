c22266b3476ebb4af3fabb89fa3fc876cd8bf3c6
Herick <herickminecraftano@gmail.com>
2024-08-12 23:42:36 -0300

    arrumar fonte personalizada em JS

--- portfolio.js
+++ portfolio.js
@@ -1,19 +1,21 @@
 // configurando botões
 // botão do homer
 function view1bt(homer){
-
+    document.getElementById("texto").innerHTML = "HOMER SIMPSON";
+    document.getElementById("texto").style.color = 'YELLOW';
+    document.getElementById("texto").style.fontFamily = 'Simpsons';
     if (homer == 1){
-        window.open("homer_simpson.html");
+        window.open("./assets/Projects/homer_simpson.html");
     }
     else if (homer == 2){
         window.open("https://github.com/NGziiin/HTML-design/blob/main/homer%20simpson.html");
     }
 }
-function mousehomer(){
-    document.getElementById("texto").innerHTML = "HOMER SIMPSON"
-}
 //botão do bart
 function view2bt(bart){
+    document.getElementById("texto").innerHTML = "BART SIMPSON";
+    document.getElementById("texto").style.color = 'YELLOW';
+    document.getElementById("texto").style.fontFamily = 'Simpsons';
     if (bart == 1){
         alert("PROJETO NÃO FINALIZADO");
     }
@@ -21,9 +23,7 @@ function view2bt(bart){
         alert("PROJETO NÃO FINALIZADO");
     }
 }
-function mousebart(){
-    document.getElementById("texto").innerHTML = "BART SIMPSON";
-}
+// tirando texto da tela
 function reset(){
     document.getElementById("texto").innerHTML = "";
   }
@@ -31,6 +31,8 @@ function reset(){
 // mensagem de em breve
 function error(x){
     document.getElementById("texto").innerHTML = "EM BREVE";
+    document.getElementById("texto").style.color = 'white';
+    document.getElementById("texto").style.fontFamily = 'arial';
     if (x == 1){
         alert ("EM BREVE");
     }
